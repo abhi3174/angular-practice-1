f5d1e164d09269a6e004d76f5725e8676b258346
koderoom <koderoom.dev@gmail.com>
2020-08-31 15:28:39 +0530

    update.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'myapp3';
+
+  inputTask = new FormControl();
+  taskList: any = [];
+
+  addTask() {
+    const newtask = this.inputTask.value;
+    this.taskList.push(newtask);
+
+    this.inputTask.setValue('');
+  }
 }
